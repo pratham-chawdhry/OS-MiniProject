4aa29d04d9aaa8eb50f2ca6f0b28c8ab8972789c
Pratham Chawdhry <pchawdhry12@gmail.com>
2024-05-19 15:42:39 +0530

    removed files

--- transactions.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSACTION_H
-#define TRANSACTION_H
-
-int issue_book(int book_id, char* username, char* password);
-int return_book(int book_id, char* username, char* password);
-int get_num_of_user_books(char* username, char* password, int arr[], int* num_of_books);
-
-#endif
